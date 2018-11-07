1962f97a9f8b3c72da86d218dd6f68e43666c97c
超悟空 <39251474@163.com>
2018-11-07 12:18:08 +0800

    写个单元测试

--- work/test/work_test.dart
+++ work/test/work_test.dart
@@ -1,5 +1,29 @@
+import 'dart:io';
+
 import 'package:work/work.dart';
 
-void main() {
+void main() async {
+  await TestWork().start();
+}
 
+class TestWork extends SimpleWork<Null> {
+  @override
+  Null onExtractResult(resultData, SimpleWorkData<Null> data) {}
+
+  @override
+  String onUrl(List params) => "http://192.168.0"
+      ".20:8080/MeetingServer/test/postParam";
+
+  @override
+  void onFillParams(Map<String, dynamic> data, List params) {
+    data["members"] = ["aaa"];
+  }
+
+  @override
+  HttpMethod get httpMethod => HttpMethod.post;
+
+  @override
+  void onConfigOptions(Options options, List params) {
+    options.contentType = ContentType.json;
+  }
 }
